cb5d1a789083143471ea69f98e59eba1f0bcf901
Elisabeth Vogel <evogel@student.unimelb.edu.au>
2018-05-19 23:49:10 +1000

    Remove package meta-data in zzz.R.

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,21 @@
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to my package")
-}
-
-
-.onLoad <- function(libname, pkgname) {
-  op <- options()
-  op.devtools <- list(
-    devtools.path = "~/R-dev",
-    devtools.install.args = "",
-    devtools.name = "Elisabeth Vogel",
-    devtools.desc.author = "Elisabeth Vogel <elisabeth.vogel@climate-energy-college.org>",
-    # devtools.desc.license = "What license is it under?",
-    devtools.desc.suggests = NULL,
-    devtools.desc = list()
-  )
-  toset <- !(names(op.devtools) %in% names(op))
-  if(any(toset)) options(op.devtools[toset])
-
-  invisible()
-}
+# .onAttach <- function(libname, pkgname) {
+#   packageStartupMessage("Welcome to my package")
+# }
+#
+#
+# .onLoad <- function(libname, pkgname) {
+#   op <- options()
+#   op.devtools <- list(
+#     devtools.path = "~/R-dev",
+#     devtools.install.args = "",
+#     devtools.name = "Elisabeth Vogel",
+#     devtools.desc.author = "Elisabeth Vogel <elisabeth.vogel@climate-energy-college.org>",
+#     # devtools.desc.license = "What license is it under?",
+#     devtools.desc.suggests = NULL,
+#     devtools.desc = list()
+#   )
+#   toset <- !(names(op.devtools) %in% names(op))
+#   if(any(toset)) options(op.devtools[toset])
+#
+#   invisible()
+# }
